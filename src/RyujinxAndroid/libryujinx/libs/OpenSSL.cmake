ce2f577f1bbfc3474ef073cf7501c2a138672945
TSR Berry <20988865+TSRBerry@users.noreply.github.com>
2024-05-01 19:22:14 +0200

    Fix issues with semicolon-separated lists

--- src/RyujinxAndroid/libryujinx/libs/OpenSSL.cmake
+++ src/RyujinxAndroid/libryujinx/libs/OpenSSL.cmake
@@ -7,7 +7,13 @@ set(PROJECT_ENV "ANDROID_NDK_ROOT=${CMAKE_ANDROID_NDK}")
 if (CMAKE_HOST_WIN32)
     find_program(PERL_COMMAND NAMES perl)
     set(PROJECT_CFG_PREFIX ${PERL_COMMAND})
-    list(APPEND PROJECT_ENV "Path=${ANDROID_TOOLCHAIN_ROOT}\\bin;$ENV{Path}")
+    # Deal with semicolon-separated lists
+    set(PROJECT_PATH_LIST $ENV{Path})
+    list(PREPEND PROJECT_PATH_LIST "${ANDROID_TOOLCHAIN_ROOT}\\bin")
+    # Replace semicolons with "|"
+    list(JOIN PROJECT_PATH_LIST "|" PROJECT_PATH_STRING)
+    # Add the modified PATH string to PROJECT_ENV
+    list(APPEND PROJECT_ENV "Path=${PROJECT_PATH_STRING}")
 elseif (CMAKE_HOST_UNIX)
     list(APPEND PROJECT_ENV "PATH=${ANDROID_TOOLCHAIN_ROOT}/bin:$ENV{PATH}")
 else ()
@@ -19,6 +25,7 @@ ExternalProject_Add(
         openssl
         GIT_REPOSITORY              https://github.com/openssl/openssl.git
         GIT_TAG                     a7e992847de83aa36be0c399c89db3fb827b0be2 # openssl-3.2.1
+        LIST_SEPARATOR              "|"
         CONFIGURE_COMMAND           ${CMAKE_COMMAND} -E env ${PROJECT_ENV}
                                     ${PROJECT_CFG_PREFIX} <SOURCE_DIR>/Configure
                                     android-${CMAKE_ANDROID_ARCH}
